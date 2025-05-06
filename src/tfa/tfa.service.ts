41fc43574f723982535cc0e85fb50bcc3b0908bb
Marshanishvili Mykhailo <sozure.synergy@gmail.com>
2025-05-06 14:17:12 +0200

    fix verify otp method

--- src/tfa/tfa.service.ts
+++ src/tfa/tfa.service.ts
@@ -50,36 +50,43 @@ export class TfaService {
   }
 
   async verify(userId: string, code: string): Promise<boolean> {
-    const blockKey = `tfa:block:${userId}`
-    const failKey = `tfa:fail:${userId}`
+		const blockKey = `tfa:block:${userId}`;
+		const failKey = `tfa:fail:${userId}`;
+	
+		const user = await this.prisma.user.findUniqueOrThrow({ where: { id: userId } });
 
-    const isBlocked = await this.redis.get(blockKey)
-    if (isBlocked) {
-      throw new ForbiddenException('Account temporarily locked due to repeated failed attempts')
-    }
-
-    const user = await this.prisma.user.findUniqueOrThrow({ where: { id: userId } })
-    if (!user.totpSecretEnc) throw new BadRequestException('TOTP not set')
-
-    const secret = this.decrypt(Buffer.from(user.totpSecretEnc))
-    const ok = authenticator.check(code, secret)
-
-    if (!ok) {
-      const fails = await this.redis.incr(failKey)
-      if (fails === 1) {
-        await this.redis.expire(failKey, ATTEMPT_TTL_SEC)
-      }
-
-      if (fails >= MAX_ATTEMPTS) {
-        await this.redis.set(blockKey, '1', 'EX', BLOCK_TTL_SEC)
-        await this.redis.del(failKey)
-        throw new ForbiddenException('Too many attempts, account temporarily locked')
-      }
-
-      throw new UnauthorizedException('Invalid code')
-    }
-
-    await this.redis.del(failKey)
-    return true
-  }
+		if (user.totpEnabled) {
+			throw new BadRequestException('TFA is already activated');
+		}
+	
+		const isBlocked = await this.redis.get(blockKey);
+		if (isBlocked) {
+			throw new ForbiddenException('Account temporarily locked due to repeated failed attempts');
+		}
+	
+		if (!user.totpSecretEnc) {
+			throw new BadRequestException('TOTP not set');
+		}
+	
+		const secret = this.decrypt(Buffer.from(user.totpSecretEnc));
+		const ok = authenticator.check(code, secret);
+	
+		if (!ok) {
+			const fails = await this.redis.incr(failKey);
+			if (fails === 1) {
+				await this.redis.expire(failKey, ATTEMPT_TTL_SEC);
+			}
+	
+			if (fails >= MAX_ATTEMPTS) {
+				await this.redis.set(blockKey, '1', 'EX', BLOCK_TTL_SEC);
+				await this.redis.del(failKey);
+				throw new ForbiddenException('Too many attempts, account temporarily locked');
+			}
+	
+			throw new UnauthorizedException('Invalid code');
+		}
+	
+		await this.redis.del(failKey);
+		return true;
+	}
 }
